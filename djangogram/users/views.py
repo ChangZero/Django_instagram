686b699f3224415fc62c778b31b1d85aace9cb4d
ChangZero <kimcy9973@gmail.com>
2022-05-12 00:46:44 +0900

    #5-1

--- djangogram/users/views.py
+++ djangogram/users/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
-from django.urls import reverse       
+from django.urls import reverse
+
+from djangogram.users.forms import SignUpForm       
 
 def main(request):
     if request.method == 'GET':
@@ -20,3 +22,25 @@ def main(request):
         else:
             # 로그인 실패시 메인화면 다시 띄움
             return render(request, 'users/main.html')
+        
+def signup(request):
+    if request.method == 'GET':
+        form = SignUpForm()
+        
+        return render(request, 'users/signup.html', {'form': form})
+    elif request.method == 'POST':
+        form = SignUpForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                # 리다이렉트로 로그인받으면 돌려보내줌
+                login(request, user)
+                return HttpResponseRedirect(reverse('posts:index'))
+            
+        return render(request, 'users/main.html')
