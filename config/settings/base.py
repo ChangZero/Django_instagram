7a692ec9d0fecde5b50fd8a9b1f0e954bd47596b
ChangZero <kimcy9973@gmail.com>
2022-05-23 00:19:14 +0900

    #10-3

--- config/settings/base.py
+++ config/settings/base.py
@@ -61,6 +61,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # APPS
 # ------------------------------------------------------------------------------
 DJANGO_APPS = [
+    "rest_framework",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
